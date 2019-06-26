dcacbe76cbe8c01ea6305741eb2f2f352593c100
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-06-26 09:11:41 +0000

    lowercase transcript

--- util/modules.py
+++ util/modules.py
@@ -68,8 +68,8 @@ def format_data(args, data):
         row['wav_filename'] = args.wav_dir + '/' + row['wav_filename'] + '.wav'
         row['wav_filesize'] = os.path.getsize(row['wav_filename'])
     
-    new_data['transcript'] = data['transcript'].lower()
-    
+    new_data['transcript'] = data['transcript'].str.lower()
+
     return new_data
 
 def export_csv(args, data, name):
