1dc23baa50595c4d452e4bd08244691d49164e04
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-06-28 17:16:51 +0000

    bæææ

--- util/modules.py
+++ util/modules.py
@@ -32,7 +32,7 @@ def filter_data(args, data):
         filters2 = data[data['transcript'].str.contains('|'.join(searchfor))].index
         data = data.drop(filters)
         data = data.drop(filters2)
-        filters3 = data[data['transcript'].str.contains(u'\ufeff')]
+        filters3 = data[data['transcript'].str.contains(u'\ufeff')].index
         data = data.drop(filters3)
 
     # Stilla ratio af noisy audio
